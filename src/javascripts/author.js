924cd961e89606aaf5e6710a288724afa2152136
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-05 18:00:49 +0200

    Add event handler for textarea changes

--- src/javascripts/author.js
+++ src/javascripts/author.js
@@ -1,1 +1,21 @@
-console.log('Hello world')
+var $ = require('zepto-browserify').$
+var debounce = require('debounce')
+
+$(function () {
+  var inputNode = $('textarea')
+  var previousValue = inputNode.val()
+  var handleChange
+
+  handleChange = debounce(function () {
+    var value = inputNode.val()
+
+    if (value !== previousValue) {
+      console.log('Value changed')
+      previousValue = value
+    }
+  }, 300)
+
+  $.each(['change', 'onpropertychange', 'input', 'keydown', 'click', 'focus', 'scroll'], function (index, eventName) {
+    inputNode.on(eventName, handleChange)
+  })
+})
