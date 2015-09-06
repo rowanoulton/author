1780379cf4007ecadd496b3d5895c70985ed4d23
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-06 14:34:43 +0200

    Add adverb detection

--- src/javascripts/author.js
+++ src/javascripts/author.js
@@ -1,21 +1,71 @@
-var $ = require('zepto-browserify').$
+var $ = require('jquery')
 var debounce = require('debounce')
+var checks = [{ fn: require('adverb-where'), name: 'adverb' }]
 
 $(function () {
   var inputNode = $('textarea')
   var highlightNode = $('.highlights')
   var previousValue = inputNode.val()
   var handleChange
+  var populateHighlighters
+  var emptyHighlighters
 
   handleChange = debounce(function () {
     var value = inputNode.val()
 
     if (value !== previousValue) {
-      console.log('Value changed')
       previousValue = value
-      highlightNode.html(value)
+      populateHighlighters(value)
     }
   }, 300)
+
+  populateHighlighters = function (value) {
+    var html = ''
+    var issues = []
+    var slices = {}
+    var previousEnd = 0
+
+    $.each(checks, function (key, checker) {
+      var found = checks[key].fn(value)
+      if (found.length) {
+        $.each(found, function (index, foundItem) {
+          issues.push({ type: checks[key].name, start: foundItem.index, end: (foundItem.index + foundItem.offset)})
+        })
+      }
+    })
+
+    if (issues.length) {
+      $.each(issues, function (index, issue) {
+        if (issue.start in slices) {
+          if (issue.end > slices[issue.start].end) {
+            slices[issue.start] = { end: issue.end, type: issue.type }
+          }
+        } else {
+          slices[issue.start] = { end: issue.end, type: issue.type }
+        }
+      })
+    }
+
+
+    $.each(slices, function(start, slice) {
+      var end = slice.end
+      var type = slice.type
+
+      html += value.slice(previousEnd, start)
+      html += '<span class="highlight highlight--' + type + '">' + value.slice(start, end) + '</span>'
+
+      previousEnd = end
+    })
+
+    html += value.slice(previousEnd, value.length)
+
+    emptyHighlighters()
+    highlightNode.html(html)
+  }
+
+  emptyHighlighters = function () {
+    highlightNode.empty()
+  }
 
   $.each(['change', 'onpropertychange', 'input', 'keydown', 'click', 'focus', 'scroll'], function (index, eventName) {
     inputNode.on(eventName, handleChange)
