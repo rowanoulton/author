9915a89f6ee57cf30a267368b052a7b3872eee16
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-09 12:08:33 +0200

    Replace submodule with npm version of adverbs library

--- src/javascripts/author.js
+++ src/javascripts/author.js
@@ -2,7 +2,7 @@ var $ = require('jquery')
 var debounce = require('debounce')
 var selection = require('./modules/save-selection')
 var checks = [
-  { fn: require('./modules/adverbs'), name: 'adverb' },
+  { fn: require('adverbs'), name: 'adverb' },
   { fn: require('passive-voice'), name: 'passive-voice' }
 ]
 
