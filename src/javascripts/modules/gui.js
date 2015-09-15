36cae65ae565b21413f26e83dbf9ddb0f83a0b79
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-15 22:09:09 +0200

    Show/hide plural "s"

--- src/javascripts/modules/gui.js
+++ src/javascripts/modules/gui.js
@@ -5,8 +5,11 @@ var pipeline = require('./pipeline')
 
 module.exports = function () {
   var sentenceCountNode = $('#sentence-count')
+  var sentenceCountPluralNode = $('#sentence-count-plural')
   var wordCountNode = $('#word-count')
+  var wordCountPluralNode = $('#word-count-plural')
   var readingTimeNode = $('#reading-time')
+  var readingTimePluralNode = $('#reading-time-plural')
   var transform
   var getWordCount
   var getSentenceCount
@@ -50,6 +53,13 @@ module.exports = function () {
     sentenceCountNode.html(numeral(sentenceCount).format('0,0'))
     wordCountNode.html(numeral(wordCount).format('0,0'))
     readingTimeNode.html(numeral(readingTime).format('0,0'))
+
+    if (sentenceCount === 1) sentenceCountPluralNode.removeClass('is-visible')
+    if (sentenceCount !== 1) sentenceCountPluralNode.addClass('is-visible')
+    if (wordCount === 1) wordCountPluralNode.removeClass('is-visible')
+    if (wordCount !== 1) wordCountPluralNode.addClass('is-visible')
+    if (readingTime === 1) readingTimePluralNode.removeClass('is-visible')
+    if (readingTime !== 1) readingTimePluralNode.addClass('is-visible')
 
     return text
   }
