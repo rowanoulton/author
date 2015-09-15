c209fe9170c8659ea74e3dd56a6fc9bb4bd134db
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-15 18:04:45 +0200

    Plumb in gui

--- src/javascripts/author.js
+++ src/javascripts/author.js
@@ -4,6 +4,7 @@ var adverbs = require('./modules/adverbs')
 var longWords = require('./modules/long-words')
 var passiveVoice = require('./modules/passive-voice')
 var sentenceLength = require('./modules/sentence-length')
+var gui = require('./modules/gui')
 var destination = require('./modules/destination')
 var Readable = require('stream').Readable
 
@@ -22,6 +23,7 @@ $(function () {
           .pipe(longWords())
           .pipe(passiveVoice())
           .pipe(sentenceLength())
+          .pipe(gui())
           .pipe(destination(highlighterNode))
 
     // Push input through
