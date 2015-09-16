cb345a4485a30e54762c4344771ef16e38f1a472
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-16 22:06:08 +0200

    Fade in/out gui lines as applicable

--- src/javascripts/modules/gui.js
+++ src/javascripts/modules/gui.js
@@ -3,13 +3,22 @@ var wordcount = require('wordcount')
 var numeral = require('numeral')
 var pipeline = require('./pipeline')
 
+$.extend($.easing, {
+  easeOutQuad: function (x, t, b, c, d) {
+    return -c * (t /= d) * (t - 2) + b
+  }
+})
+
 module.exports = function () {
-  var sentenceCountNode = $('#sentence-count')
-  var sentenceCountPluralNode = $('#sentence-count-plural')
-  var wordCountNode = $('#word-count')
-  var wordCountPluralNode = $('#word-count-plural')
-  var readingTimeNode = $('#reading-time')
-  var readingTimePluralNode = $('#reading-time-plural')
+  var sentenceCountWrapNode = $('#sentence-count')
+  var sentenceCountNode = sentenceCountWrapNode.find('.gui-counter-value')
+  var sentenceCountPluralNode = sentenceCountWrapNode.find('.gui-counter-plural')
+  var wordCountWrapNode = $('#word-count')
+  var wordCountNode = wordCountWrapNode.find('.gui-counter-value')
+  var wordCountPluralNode = wordCountWrapNode.find('.gui-counter-plural')
+  var readingTimeWrapNode = $('#reading-time')
+  var readingTimeNode = readingTimeWrapNode.find('.gui-counter-value')
+  var readingTimePluralNode = readingTimeWrapNode.find('.gui-counter-plural')
   var transform
   var getWordCount
   var getSentenceCount
@@ -52,12 +61,21 @@ module.exports = function () {
     wordCountNode.html(numeral(wordCount).format('0,0'))
     readingTimeNode.html(numeral(readingTime).format('0,0'))
 
+    // Show/hide plural "s"
     if (sentenceCount === 1) sentenceCountPluralNode.removeClass('is-visible')
     if (sentenceCount !== 1) sentenceCountPluralNode.addClass('is-visible')
     if (wordCount === 1) wordCountPluralNode.removeClass('is-visible')
     if (wordCount !== 1) wordCountPluralNode.addClass('is-visible')
     if (readingTime === 1) readingTimePluralNode.removeClass('is-visible')
     if (readingTime !== 1) readingTimePluralNode.addClass('is-visible')
+
+    // Show/hide gui line
+    if (sentenceCount === 0) sentenceCountWrapNode.stop().fadeOut({duration: 300, easing: 'easeOutQuad'})
+    if (sentenceCount !== 0) sentenceCountWrapNode.stop().fadeIn({duration: 900, easing: 'easeOutQuad'})
+    if (wordCount === 0) wordCountWrapNode.stop().fadeOut({duration: 300, easing: 'easeOutQuad'})
+    if (wordCount !== 0) wordCountWrapNode.stop().fadeIn({duration: 900, easing: 'easeOutQuad'})
+    if (readingTime === 0) readingTimeWrapNode.stop().fadeOut({duration: 300, easing: 'easeOutQuad'})
+    if (readingTime !== 0) readingTimeWrapNode.stop().fadeIn({duration: 900, easing: 'easeOutQuad'})
 
     return text
   }
